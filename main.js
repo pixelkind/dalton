2c92c4f92effe2ec331855e907b5ad2c40f6cbbe
pixelkind <github@pixelkind.com>
2016-10-31 08:45:03 +0100

    Toggle Developer Tools added to menu

--- main.js
+++ main.js
@@ -73,7 +73,9 @@ function createWindow () {
             { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
             { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
             { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" },
+            { type: "separator" },
+            { label: "Toggle Developer Tools", click: function() { mainWindow.webContents.toggleDevTools(); } }
         ]}, {
           role: 'window',
           submenu: [{
